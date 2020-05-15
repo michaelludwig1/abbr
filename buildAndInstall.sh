5ebc1339e4ae172269340321e1cbe5ce1ecc3f9c
Michael Ludwig <mail@michael-ludwig.eu>
2020-05-15 22:11:07 +0200

    Folder generation for installer

--- buildAndInstall.sh
+++ buildAndInstall.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 go build abbrbin.go
+
+mkdir -p ~/.local
+mkdir -p ~/.local/bin
+mkdir -p ~/.config
+mkdir -p ~/.config/abbr
+
 rm ~/.local/bin/abbrbin 2> /dev/null
 cp ./abbrbin ~/.local/bin/abbrbin
 chmod +x ~/.local/bin/abbrbin
@@ -9,7 +15,6 @@ rm ~/.local/bin/abbr.sh 2> /dev/null
 cp ./abbr.sh ~/.local/bin/abbr.sh
 chmod +x ~/.local/bin/abbr.sh
 
-mkdir -p ~/.config/abbr
 touch ~/.config/abbr/aliases
 
 if ! grep -q "alias abbr" ~/.bashrc; then
